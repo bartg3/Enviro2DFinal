71d48efa108d2573c58177b708c08a3b28157671
LBentes <60331057+LBentes@users.noreply.github.com>
2021-04-26 12:33:26 -0400

    Main Menu

---
+++ Project/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    public void QuitTheGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
